973bccf6d1221169f598ad13c84cc1eda3486828
Ming Zhang <Stonetiger1225@gmail.com>
2019-12-07 10:59:05 +0800

    initial commit
    initial commit

---
+++ Assets/Script/Perfect_effect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perfect_effect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite[] spriteImage = new Sprite[6];
+    public Transform ball;
+    private float alpha = 0;
+    private bool ballIsDestroyed = false;
+    void UpdateEffect(int perfectNum)
+    {
+        if (perfectNum > 3 && perfectNum < 9)
+        {
+            alpha = 1f;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, alpha);
+            gameObject.GetComponent<SpriteRenderer>().sprite = spriteImage[perfectNum - 4];
+        }
+        if (perfectNum > 8)
+        {
+            alpha = 1f;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, alpha);
+            gameObject.GetComponent<SpriteRenderer>().sprite = spriteImage[5];
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballIsDestroyed == false)
+            transform.position = ball.position + Vector3.up * 1f;
+        if (alpha > 0)
+        {
+            alpha -= 0.02f;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, alpha);
+        }
+    }
+
+    public void BallIsDestroyed()
+    {
+        ballIsDestroyed = true;
+    }
+}
---
+++ Assets/Script/RestartButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+}
